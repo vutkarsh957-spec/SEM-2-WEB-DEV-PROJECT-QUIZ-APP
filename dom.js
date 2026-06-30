95a4db5ac7e6c99bbff840a3364c408093e63a10
mdshahnawazshamim <shahnawaz.shamim@outlook.com>
2026-06-30 13:30:51 +0530

    Create dom.js to manage DOM elements for quiz

---
+++ dom.js
@@ -0,0 +1,7 @@
+export const dom = {
+  questionText: document.getElementById('question'),
+  optionsBox: document.getElementById('options-container'),
+  nextBtn: document.getElementById('next-btn'),
+  progressTracker: document.getElementById('score-tracker'),
+  quizCard: document.getElementById('quiz-card')
+};
